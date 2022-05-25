e9e8205c5d9d7cba5a8d9d5b9dd613ff26724f47
Oliver Scharkowski <59687448+oscharko@users.noreply.github.com>
2022-05-25 15:41:15 +0000

    Another solution with one for loop... the same result... time limit 😒

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 let nums = [-1,-100,3,99], k = 2
 // [3,99,-1,-100]
 
-function arrayRotate(nums, k) {
-  /*
-  # Description for array v:
-  #   - v[0] = runner
-  #   - v[1] = input original array and also output rotate array
-  #   - v[2] = rotate steps
-  */
-  let v = [0,nums,k]
-  while(v[0] < v[2]){
-    v[1].unshift(v[1].pop());
-    v[0]++
+const rotate = function(nums, k) {
+  for(i = 0; i < k; i++) {
+    nums.unshift(nums.pop());
   }
-  return v[1];
+  return nums;
 }
 
-console.log(arrayRotate(nums, k))
+console.log(rotate(nums, k))
